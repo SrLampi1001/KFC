415961ad49253d0afb1d1217fafce88d7ae5f7c2
SrLampi1001 <sanchezsantiago1001@gmail.com>
2026-01-06 16:55:18 -0500

    added simple sesion verification and redirection

--- script/log_in.js
+++ script/log_in.js
@@ -6,11 +6,12 @@ let correct_email = "Email@gmail.com";
 document.getElementById("btn-submit").addEventListener("click", (e)=>{
     if (password_input.value == correct_password && correct_email == email_input.value){
         // Functionality
+        sessionStorage.setItem("email", email_input);
         window.location = "../index.html";
     } else{
         password_input.value = "";
         message = document.createElement("span"); message.textContent = "La contraseña o correo incorrecto";
-        message.setAttribute("style", "position: absolute; top:50%; left:50%; transform:translate(-50%, 50%); display:inline-block; width:max-content; color:red");
+        message.setAttribute("style", "position: absolute; left:50%; transform:translate(-50%, 50%); display:inline-block; width:max-content; color:red");
         document.querySelector("body").append(message)
         setTimeout(() => {
             message.remove()
