25a732d1e4ec0ae2e82af5fae2c829dfb6527ac5
SrLampi1001 <sanchezsantiago1001@gmail.com>
2026-01-05 21:25:50 -0500

    first version

---
+++ script/log_in.js
@@ -0,0 +1,20 @@
+const password_input = document.getElementById("password");
+const email_input = document.getElementById("email");
+let correct_password = "PASWORDSSSSS";
+let correct_email = "Email@gmail.com";
+
+document.getElementById("btn-submit").addEventListener("click", (e)=>{
+    if (password_input.value == correct_password && correct_email == email_input.value){
+        // Functionality
+        window.location = "../index.html";
+    } else{
+        password_input.value = "";
+        message = document.createElement("span"); message.textContent = "La contraseña o correo incorrecto";
+        message.setAttribute("style", "position: absolute; top:50%; left:50%; transform:translate(-50%, 50%); display:inline-block; width:max-content; color:red");
+        document.querySelector("body").append(message)
+        setTimeout(() => {
+            message.remove()
+        }, 2000);
+
+    }
+})
